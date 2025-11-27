63911c25e2871fd5832f37d6c09cc1ab3cbc35ac
Oleh Zinchenko <olegzinchenko1983@gmail.com>
2025-11-27 21:38:00 +0000

    Using the Context API for State Management

--- app/_components/DateSelector.js
+++ app/_components/DateSelector.js
@@ -4,6 +4,8 @@ import { isWithinInterval } from 'date-fns'
 import { DayPicker } from 'react-day-picker'
 import 'react-day-picker/dist/style.css'
 
+import { useReservation } from '@/app/_components/ReservationContext'
+
 function isAlreadyBooked(range, datesArr) {
   return (
     range.from &&
@@ -15,12 +17,14 @@ function isAlreadyBooked(range, datesArr) {
 }
 
 function DateSelector({ cabin, settings, bookedDates }) {
+  // const [range, setRange] = useState({ from: undefined, to: undefined })
+  const { range, setRange, resetRange } = useReservation()
+
   // CHANGE
   const regularPrice = 23
   const discount = 23
   const numNights = 23
   const cabinPrice = 23
-  const range = { from: null, to: null }
 
   // SETTINGS
   const { minBookingLength, maxBookingLength } = settings
@@ -30,6 +34,8 @@ function DateSelector({ cabin, settings, bookedDates }) {
       <DayPicker
         className='pt-12 place-self-center'
         mode='range'
+        onSelect={setRange}
+        selected={range}
         min={minBookingLength + 1}
         max={maxBookingLength}
         fromMonth={new Date()}
@@ -70,7 +76,7 @@ function DateSelector({ cabin, settings, bookedDates }) {
         {range.from || range.to ? (
           <button
             className='border border-primary-800 py-2 px-4 text-sm font-semibold'
-            onClick={() => resetRange()}
+            onClick={resetRange}
           >
             Clear
           </button>
